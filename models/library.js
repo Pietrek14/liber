7a72f418434b257184c7f97fd4114fc9cb4e2241
Pietrek14 <dawidekpe@gmail.com>
2021-04-24 20:48:11 +0200

    Dodałem bazę danych

--- models/library.js
+++ models/library.js
@@ -1,1 +1,12 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const librarySchema = new Schema({
+	name: String,
+	librarian: Number,	// ID bibliotekarza
+	books: [Number]	// ID książek
+});
+
+const library = mongoose.model("library", librarySchema);
+
+module.exports = library;
