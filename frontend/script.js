2873b0a964bdc3c08555dd5ce801347267162a45
Pietrek14 <dawidekpe@gmail.com>
2021-05-13 15:34:57 +0200

    coś tam zrobiłem

--- frontend/script.js
+++ frontend/script.js
@@ -2,20 +2,23 @@ import { serverAddress } from "./scripts/constants.js";
 
 const content = document.getElementById("content");
 
-const res = await fetch(`${serverAddress}/getname`, {
-	method: "GET",
-	credentials: "include",
-	headers: {
-		"Content-Type": "application/json",
-	},
-});
+const init = async () => {
+	const res = await fetch(`${serverAddress}/getname`, {
+		method: "GET",
+		credentials: "include",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
 
-const data = await res.json();
+	const data = await res.json();
 
-if (res.status === 401) {
-	// window.location = "./login/";
-	console.log(data.message);
-}
+	if (res.status === 401) {
+		window.location = "./login/";
+	}
+};
+
+init();
 
 const name = data.name;
 
