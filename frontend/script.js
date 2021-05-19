e154c2c4d6b8f4e62cbbbbbe06043147aa6991b6
Pietrek14 <dawidekpe@gmail.com>
2021-05-19 08:11:56 +0200

    dodałem side bar

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,11 @@
 import { serverAddress } from "./scripts/constants.js";
 
 const content = document.getElementById("content");
+const borgarMenu = document.getElementById("borgar-menu");
+const sideBar = document.getElementById("side-bar");
+const sideBarClose = document.getElementById("side-bar-close");
+
+let sideBarOpened = false;
 
 const init = async () => {
 	const res = await fetch(`${serverAddress}/getname`, {
@@ -16,10 +21,30 @@ const init = async () => {
 	if (res.status === 401) {
 		window.location = "./login/";
 	}
+
+	const name = data.name;
+
+	content.innerText = `Witaj, ${name}!`;
 };
 
 init();
 
-const name = data.name;
+function openSideBar() {
+	borgarMenu.classList.add("active");
+	sideBar.classList.add("active");
+	sideBarOpened = true;
+}
 
-content.innerText = `Witaj, ${name}!`;
+function closeSideBar() {
+	borgarMenu.classList.remove("active");
+	sideBar.classList.remove("active");
+	sideBarOpened = false;
+}
+
+function toggleSideBar() {
+	if (sideBarOpened) closeSideBar();
+	else openSideBar();
+}
+
+borgarMenu.onclick = openSideBar;
+sideBarClose.onclick = closeSideBar;
