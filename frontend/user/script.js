aaa969795dae8592c9a640950d797c922830578c
imustend <plochockitymon@gmail.com>
2021-06-02 20:37:09 +0200

    dziala ale nie umiem html wiec nie ma css ani html

--- frontend/user/script.js
+++ frontend/user/script.js
@@ -5,6 +5,12 @@ const sideBar = document.getElementById("side-bar");
 const sideBarClose = document.getElementById("side-bar-close");
 
 const logoutButton = document.getElementById("logout-button");
+const logoutButton2 = document.getElementById("logout-button-2");
+const nameContent = document.getElementById("name");
+const emailContent = document.getElementById("email");
+const nameChageButton = document.getElementById("namechange-button");
+const passChageButton = document.getElementById("passchange-botton");
+
 
 // dane uzytkownika z serwera na jego ekran:)
 const init = async () => {
@@ -19,7 +25,7 @@ const init = async () => {
 	const data1 = await res1.json();
 
 	if (res1.status === 401) {
-		window.location = "./login/";
+		window.location = "../login/";
 	}
 
 	const res2 = await fetch(`${serverAddress}/getemail`, {
@@ -33,14 +39,15 @@ const init = async () => {
 	const data2 = await res2.json();
 
 	if (res2.status === 401) {
-		window.location = "./login/";
+		window.location = "../login/";
 	}
 
 	const name = data1.name;
 	const email = data2.email;
 
 	console.log(`Witaj, ${name}! o email ${email}`);
-	// content.innerText = `Witaj, ${name}! o email ${email}`;
+	nameContent.innerText = `Nazwa: ${name}`;
+	emailContent.innerText = `Email: ${email}`;
 };
 
 init();
@@ -73,11 +80,40 @@ logoutButton.onclick = async () => {
 	});
 
 	if (res.status === 200) {
-		window.location = "./login/";
+		window.location = "../login/";
 		return;
 	}
 
 	const data = await res.json();
 
 	alert(data.message);
+};
+
+logoutButton2.onclick = async () => {
+	const res = await fetch(`${serverAddress}/logout`, {
+		method: "POST",
+		credentials: "include",
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+
+	if (res.status === 200) {
+		window.location = "../login/";
+		return;
+	}
+
+	const data = await res.json();
+
+	alert(data.message);
+};
+
+// zmiana nazwy
+nameChageButton.onclick = async () => {
+	window.location.replace("../nameChange");
+};
+
+// zmaina hasla
+passChageButton.onclick = async () => {
+	window.location.replace("../passwordForgot");
 };
