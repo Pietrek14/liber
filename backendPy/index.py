b1aff4fa92ee6eb20fdeb41f360dde061f63f12c
Lulando <pawellulewicz39@gmail.com>
2021-06-12 20:07:24 +0200

    ok

--- backendPy/index.py
+++ backendPy/index.py
@@ -1,14 +1,13 @@
 # przed uruchomieniem nalezy wlaczyc virtual enviroment
-# komenda env/Script/activate !!!
+# komenda flask/Script/activate !!!
 
 from flask import Flask, request, make_response
-import endpoints.testing as testing
 
 app = Flask(__name__)
 
-@app.route('/test', methods=['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return testing.test()
+    return "yes"
 
 if __name__ == '__main__':  
     app.run(debug=True)
